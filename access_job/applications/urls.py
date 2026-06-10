71dcd2db5914b729657fd312a7a830a235de74a4
usamaa757 <usamaahmad757@gmail.com>
2026-06-10 23:50:13 +0200

    Delete access_job directory

--- access_job/applications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "applications"
-
-urlpatterns = [
-    path("applications/", views.application_list, name="application-list"),
-]
--- access_job/recomendations/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Recommendation
-
-
-class RecommendationForm(forms.ModelForm):
-    class Meta:
-        model = Recommendation
-        fields = ["model_type", "score", "reason"]
